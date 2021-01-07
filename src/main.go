7d7b343b095e62c2270bcf0afd11f4a7e7dc2d28
cyouho <cyouho@hotmail.co.jp>
2021-01-07 13:01:23 +0900

    feature/001_テストする

--- src/main.go
+++ src/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
+	var imageURL string = "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2965081049,3975994462&fm=26&gp=0.jpg"
+	baiduImages(imageURL)
 	fmt.Println("Hello, 世界")
+}
+
+func baiduImages(imageURL string) {
+
 }
